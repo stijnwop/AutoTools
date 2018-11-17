98d9a06b792d86ec4be8b3d5c19171e8d006a4d7
Stijn Wopereis <stijnwop@gmail.com>
2018-11-17 19:40:24 +0100

    Update util and main class

--- AutoTools.py
+++ AutoTools.py
@@ -10,41 +10,60 @@
 import inspect
 import sys
 
+from gui.MainWindow import MainWindow
+from gui.MQtUtil import getMainWindow, getPointerForWidget
+
 from os.path import dirname
 
-from gui.MainWindow import MainWindow
-from gui.MQtUtil import getMainWindow
+import logging
 
-ROOT_DIR = dirname(__file__)
+logging.basicConfig()
+
+_ROOT_DIR = dirname(__file__)
 
 
 class AutoTool:
 
     def __init__(self):
-        main = MainWindow()
-        main.show()
-        
+        self.main_window = MainWindow(getMainWindow())
+        self.main_window.show()
+
+    def delete(self):
+        if not self.main_window is None:
+            self.main_window.deleteLater()
+
 
 def resetSessionForScript(userPath=None):
     if userPath is None:
-        userPath = ROOT_DIR
+        userPath = _ROOT_DIR
 
     userPath = userPath.lower()
 
+    print(userPath)
     toDelete = []
 
     for key, module in sys.modules.iteritems():
         try:
             moduleFilePath = inspect.getfile(module).lower()
 
+            logging.info("Current %s" % __file__.lower())
+
             if moduleFilePath == __file__.lower():
                 continue
 
             if moduleFilePath.startswith(userPath):
-                print("Removing %s" % key)
+                logging.info("Removing %s" % key)
                 toDelete.append(key)
         except:
             pass
 
     for module in toDelete:
         del (sys.modules[module])
+
+
+try:
+    main.delete()
+except NameError as e:
+    pass
+
+main = AutoTool()
--- gui/MQtUtil.py
+++ gui/MQtUtil.py
@@ -6,17 +6,25 @@
 #
 # Copyright (c) Stijn Wopereis, 2018
 
+import logging
+logging.basicConfig()
+
 import maya.cmds as cmds
 import maya.OpenMayaUI as mui
 
-from Qt import QtCore
+from os.path import dirname, abspath, realpath, sep
+
+from Qt import QtWidgets, QtCore, QtGui
 
 import shiboken2 as shiboken
+
+_ROOT_DIR = dirname(realpath(__file__))
 
 
 def getMainWindow():
     ptr = mui.MQtUtil.mainWindow()
-    return shiboken.wrapInstance(long(ptr), QtCore.QObject)
+    if ptr is not None:
+        return shiboken.wrapInstance(long(ptr), QtWidgets.QWidget)
 
 
 def getWidgetUnderPointer():
@@ -25,15 +33,63 @@ def getWidgetUnderPointer():
         return None
 
     ptr = mui.MQtUtil.findControl(panel)
-    widget = shiboken.wrapInstance(long(ptr), QtCore.QObject)
+    widget = shiboken.wrapInstance(long(ptr), QtWidgets.QWidget)
     return widget
 
 
 def getWidgetByName(name):
     ptr = mui.MQtUtil.findControl(name)
-    widget = shiboken.wrapInstance(long(ptr), QtCore.QObject)
-    return widget
+    return shiboken.wrapInstance(long(ptr), QtWidgets.QWidget)
 
 
 def getPointerForWidget(widget):
     return shiboken.getCppPointer(widget)
+
+
+def deleteControl(control):
+    if cmds.workspaceControl(control, q=True, exists=True):
+        cmds.workspaceControl(control, e=True, close=True)
+        cmds.deleteUI(control, control=True)
+
+
+def addControl(control, label, width):
+    return cmds.workspaceControl(control, ttc=["AttributeEditor", -1], label=label)
+
+# noinspection PyBroadException
+def deleteUI(name):
+    try:
+        cmds.deleteUI(name)
+        logger.info('removed workspace {}'.format(dialog_class.CONTROL_NAME))
+    except:
+        pass
+
+# noinspection PyBroadException
+def dockWindow(QtClass):
+    try:
+        cmds.deleteUI(QtClass.control_name)
+        # logger.info('removed workspace {}'.format(dialog_class.CONTROL_NAME))
+    except:
+        pass
+
+    # deleteControl(QtClass.control_name)
+
+    # building the workspace control with maya.cmds
+    main_control = addControl(QtClass.control_name, QtClass.label_name, QtClass.width)
+    # conver the C++ pointer to Qt object we can use
+    control_wrap = getWidgetByName(QtClass.control_name)
+    # control_wrap is the widget of the docking window and now we can start working with it:
+    control_wrap.setAttribute(QtCore.Qt.WA_DeleteOnClose)
+
+    win = QtClass(control_wrap)
+    wrap_win = control_wrap.window()
+
+    wrap_win.setWindowIcon(QtGui.QIcon(_ROOT_DIR + sep + "icon.png"))
+
+    # after maya is ready we should restore the window since it may not be visible
+    cmds.evalDeferred(lambda *args: cmds.workspaceControl(main_control, e=True, rs=True))
+
+    return win.run()
+
+
+def getNormalizedPath(path=""):
+    return abspath(_ROOT_DIR + path)
